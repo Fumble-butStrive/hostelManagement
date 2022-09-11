1472b154e7b0918e5c8146e5db34680f6f1424ba
shavvan burrell <shavvanburrell@gmail.com>
2022-09-10 19:21:35 -0700

    contact changes

--- controllers/contactReply.controller.js
+++ controllers/contactReply.controller.js
@@ -1,12 +1,12 @@
-const ContactReply  =  require('../models/ContactReply.model');
+const contactReply  =  require('../models/contactReply.model');
 
 async function insertContactReply(contactReply) {
     // console.log(`saving contactReply to db`, contactReply);
-    return await new ContactReply(contactReply).save();
+    return await new contactReply(contactReply).save();
 }
 
 async function contactReplyHistory() {
-    let contactReplyHistory = await ContactReply.find({});
+    let contactReplyHistory = await contactReply.find({});
     if(contactReplyHistory) {
         return contactReplyHistory;
     }
